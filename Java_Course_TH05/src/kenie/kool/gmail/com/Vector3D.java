7241b31a0a8db9b8da05ea5c5a80741a0bf36f14
nguyenvanthanhloc <37960992+nguyenvanthanhloc@users.noreply.github.com>
2018-05-05 16:31:33 +0700

    Update Vector3D.java

--- Java_Course_TH05/src/kenie/kool/gmail/com/Vector3D.java
+++ Java_Course_TH05/src/kenie/kool/gmail/com/Vector3D.java
@@ -1,5 +1,19 @@
 package kenie.kool.gmail.com;
 
 public class Vector3D extends Vector2D {
+	private float z;
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+
+	public Vector3D(float x, float y, float z) {
+		super(x,y);
+		this.z = z;
+	}
 
 }
